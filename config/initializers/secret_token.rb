96e078ad8f95a1beb04050f26c7947ba921fb7a0
Paul Kwiatkowski <paul@groupraise.com>
2014-10-12 03:13:00 -0500

    Initial Rails Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetupCalendarSync::Application.config.secret_key_base = 'f3bb63370ab625e3242b41604224365e3db0b7b333c3717eca7cfdac344070451c81539bb45d96d24c7b0d83639081b7b43f8d296b17e28e1a385e944d11bc4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MeetupCalendarSync::Application.config.session_store :cookie_store, key: '_houston-tech-calendar_session'
